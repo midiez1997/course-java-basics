527327d74c1abbbc06a0c55f4517133b3e0f53bc
Dmitry Sedun <sedun.dima@ya.ru>
2020-10-19 14:20:23 +0300

    feat: add solution for Task01.

--- src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtil.java
+++ src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtil.java
@@ -3,30 +3,34 @@ package com.rakovets.course.javabasics.practice.strings;
 public class StringUtil {
     public static void main(String[] args) {
         System.out.println(getConcat());
-        System.out.println(getequals(String string));
+        System.out.println(getIndex());
+        System.out.println(getEquals());
+        System.out.println(getTrim());
+        System.out.println(getChars());
+        System.out.println(getReplace());
     }
-    String getConcat() { // таск 1 конкатенация/ join(объединение) +
+    static String getConcat() { // таск 1 конкатенация/ join(объединение) +
      String str1 = "Hello";
      String str2 = "Dmitry Rakovets";
      String str3 = String.join(" ", str1, str2);
      System.out.println(str3);
- }
-    void index() { // таск 2 поиск быквы +
+     }
+    static String getIndex() { // таск 2 поиск буквы +
      String foundx = "this sentense include x";
      int index = foundx.indexOf('x');
      System.out.println("Индекс данной буквы: " + index);
     }
-     String getequals() { // таск 3 сравнение двух строк equals(без учета регистра) +
+     static String getEquals() { // таск 3 сравнение двух строк equals(без учета регистра) +
         String str1 = "ORDINARY WORD";
         String str2 = "ordinary word";
         System.out.println(str1.equals(str2));
     }
-    void trim() { // таск 4 trim и UpperCase +
+    static String getTrim() { // таск 4 trim и UpperCase +
         String str = " Java Basic ";
         str = str.trim();
         System.out.println(str.toUpperCase());
     }
-    void getChars() { // таск 5 +
+    static String getChars() { // таск 5 +
         String str = "два фламинго стоят на тонких ногах";
         int start = 10;
         int end = 32;
@@ -34,14 +38,42 @@ public class StringUtil {
         str.getChars(start, end, dst, 0);
         System.out.println(dst);
     }
-    void replace() { // таск 6 замена +
+    static String getReplace() { // таск 6 замена +
         String str = "Listen joy division - it is :( :( :(";
         String replStr1 = str.replace(":(", ":)");
         System.out.println(replStr1);
     }
 
+                                 // таск 7 пока непонятен
 
+    static  String getVowels() { // таск 8
 
+    }
+
+    static String getQuantity() {
+        String string = "В зада,нной. .строке . ,посчита,ть, количество т,очек, зап.я,тых и !!! знаков";
+        int drops = 0;
+        int comma = 0;
+        int exclamation = 0;
+
+        for (char ch : string.toCharArray()) { //разбиваем на массив
+            if (ch == '.') {
+                drops++;
+            }
+            else {
+                if (ch == ',') {
+                    comma++;
+                }
+            }
+            else {
+                if (ch == '!') {
+                    exclamation++
+                }
+            }
+        }
+
+        System.out.println("количество точек: " + drops + "  запятых: " + comma);
+    }
 }
 
 
