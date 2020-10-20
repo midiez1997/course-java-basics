2c90ce5e5f2ec0888113496a408f9aba9f803f95
Dmitry Sedun <sedun.dima@ya.ru>
2020-10-20 16:37:34 +0300

    add feature for Task StringUtil

--- src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtil.java
+++ src/main/java/com/rakovets/course/javabasics/practice/strings/StringUtil.java
@@ -1,77 +1,125 @@
 package com.rakovets.course.javabasics.practice.strings;
 
 public class StringUtil {
-    public static void main(String[] args) {
-        System.out.println(getConcat());
-        System.out.println(getIndex());
-        System.out.println(getEquals());
-        System.out.println(getTrim());
-        System.out.println(getChars());
-        System.out.println(getReplace());
+
+    public String getUpperCase(String string) {
+        return string.toUpperCase();
     }
-    static String getConcat() { // таск 1 конкатенация/ join(объединение) +
-     String str1 = "Hello";
-     String str2 = "Dmitry Rakovets";
-     String str3 = String.join(" ", str1, str2);
-     System.out.println(str3);
+
+    public String getConcat(String str1, String str2) { // таск 1 join(объединение) +
+     return String.join(" ", str1, str2);
      }
-    static String getIndex() { // таск 2 поиск буквы +
-     String foundx = "this sentense include x";
-     int index = foundx.indexOf('x');
-     System.out.println("Индекс данной буквы: " + index);
-    }
-     static String getEquals() { // таск 3 сравнение двух строк equals(без учета регистра) +
-        String str1 = "ORDINARY WORD";
-        String str2 = "ordinary word";
-        System.out.println(str1.equals(str2));
-    }
-    static String getTrim() { // таск 4 trim и UpperCase +
-        String str = " Java Basic ";
-        str = str.trim();
-        System.out.println(str.toUpperCase());
-    }
-    static String getChars() {    // таск 5 +
-        String str = "два фламинго стоят на тонких ногах";
-        int start = 10;
-        int end = 32;
-        char[] dst = new char[end - start];
-        str.getChars(start, end, dst, 0);
-        System.out.println(dst);
-    }
-    static String getReplace() { // таск 6 замена +
-        String str = "Listen joy division - it is :( :( :(";
-        String replStr1 = str.replace(":(", ":)");
-        System.out.println(replStr1);
+
+    public int getStringIndex(String input, String charToCheck) { // таск 2 поиск буквы +
+     return input.indexOf(charToCheck.toUpperCase());
     }
 
-                                 // таск 7 пока непонятен
-
-    static  String getVowels() { // таск 8
-
+     public boolean getEquals(String str1, String str2) { // таск 3 сравнение двух строк equals(без учета регистра) +
+        return str1.equals(str2);
     }
 
-    static String getQuantity() {
-        String string = "В зада,нной. .строке . ,посчита,ть, количество т,очек, зап.я,тых и !!! знаков";
-        int drops = 0;
-        int comma = 0;
-        int exclamation = 0;
+     public String getUpperCaseandTrim(String str) { // таск 4 trim и UpperCase +
+        return str.trim().toUpperCase();
+    }
 
-        for (char ch : string.toCharArray()) { //разбиваем на массив
-            if (ch == '.') {
-                drops++;
+    public String getStringSubstring(String str) {
+        if (str.length() < 10) {
+            return "<10";
+        }
+        String str2 = str.substring(10);
+        if (str2.length() > 23) {
+            String str3 = str2.substring(0, 23);
+            return str3;
+        }
+        return str2;
+    }
+
+    public String getReplace(String str) { // таск 6 замена +
+        return str.replace(":(", ":)");
+    }
+
+    public boolean getStringCompare(String str, String word) { // таск 7
+        if (str.startsWith(word) && str.endsWith(word)) {
+            return true;
+        }
+        return false;
+    }
+
+    public int getStringNumbersOfVowels(String str) { // таск 8
+        int count = 0;
+        char[] array = new char[]{'e', 'y', 'u', 'i', 'o', 'a'};
+        for (char simb : str.toLowerCase().toCharArray()) {
+            for (char vowel : array) {
+                if (simb == vowel) {
+                    count++;
+                    break;
+                }
             }
-            if (ch == ',') {
-                comma++;
-            }
-            if (ch == '!') {
-                exclamation++;
-            }
+        }
+        return count;
+    }
 
+    public int getStringNumbersOfSimbols(String str) { // таск 9
+        int count = 0;
+        char[] array = new char[]{'.', ',', '!'};
+        for (char simb : str.toLowerCase().toCharArray()) {
+            for (char vowel : array) {
+                if (simb == vowel) {
+                    count++;
+                    break;
+                }
+            }
+        }
+        return count;
+    }
+
+    public String getStringRevers(String strB) { // таск 10
+        StringBuilder str = new StringBuilder(strB);
+        return str.reverse().toString();
+    }
+
+    public String[] getStringArraysNumbers(String str, int n) { // таск 11
+
+        int substr = str.length() / n;
+        if (str.length() % n != 0) {
+            substr++;
+        }
+        String[] strarray = new String[substr];
+        for (int i = 0; i < substr; i++) {
+            strarray[i] = str.substring(i * n, Math.min((i + 1) * n, str.length()));
         }
 
-        System.out.println("количество точек: " + drops + "  запятых: " + comma + " восклицательных знаков: " + exclamation);
-    } //таск 9 посчитать . , ! +
+        return strarray;
+    }
 
+    public int getStringNumbersOfWords(String str) { // таск 12
+        int count = 0;
+        String[] words = str.trim().replaceAll(" +", " ").split(" +");
+
+        for (String word : words) {
+            count++;
+        }
+        return count;
+    }
+
+    public String[] getAbbreviation(String str) { // таск 13
+        String[] fio = str.toUpperCase().split(" ");
+        for (int i = 0; i < fio.length; i++) {
+            if (i != fio.length - 1) {
+                fio[i] = fio[i].charAt(0) + ".";
+            } else fio[i] = String.valueOf(fio[i].charAt(0));
+        }
+        return fio;
+    }
+
+    public String getStringNumbers(String str){ // таск 14
+        String[] numbers = str.split("\\D");
+        String result = "";
+        for (int i = 0; i<numbers.length; i++){
+            result = result + numbers[i];
+        }
+        return result;
+    }
 
 }
 
