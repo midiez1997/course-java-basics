bd824bc9764f3517fc970cc05b5a5e93999a5a51
Dmitry Sedun <sedun.dima@ya.ru>
2020-10-21 09:15:25 +0300

    feat: add solution for Task01.

--- src/test/java/com/rakovets/course/javabasics/practice/oop/inheritanceandpolymorphism/CatUtilTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/oop/inheritanceandpolymorphism/CatUtilTest.java
@@ -1,4 +1,15 @@
 package com.rakovets.course.javabasics.practice.oop.inheritanceandpolymorphism;
 
+import org.junit.jupiter.api.Test;
+
 public class CatUtilTest {
+
+    @Test
+    public void catCreationTest() {
+        Cat tom = new Cat("tom");
+        System.out.println(tom.mew());
+        Sphynx egypt = new Sphynx("egypt");
+        System.out.println(egypt.mew());
+
+    }
 }
