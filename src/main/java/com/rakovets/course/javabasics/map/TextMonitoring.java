4a63dd97994791bfd688447a3f6a0903bfff687b
Dmitry Sedun <sedun.dima@ya.ru>
2020-10-29 11:19:55 +0300

    add feature for Task TextMonitoring

---
+++ src/main/java/com/rakovets/course/javabasics/map/TextMonitoring.java
@@ -0,0 +1,22 @@
+package com.rakovets.course.javabasics.map;
+
+public class TextMonitoring {
+
+//    public String researchText(String string) {
+//
+//    }
+
+//    === Specification of task 2
+//            * Добавить метод `researchText()`, принимающий в качестве параметра текст.
+//* Метод выделяет все различные слова и подсчитывае частоту с которой они встречаются.
+//            * Слова считаются различными даже если у них отличается регистром букв.
+////      Specification of task 3
+//    Добавить методы:
+//- `getCountUniqueWords()` возвращает количество уникальных слов в тексте
+//- `getUniqueWords()` возвращает коллекцию уникальных слов из текста
+//- `get(work)` возвращае частоту употребления слова `word`
+//- `getWordFrequencyDesc(boolean isAscendingFrequency)` возвращает все слова и частоту их употребления,
+//                               `isAscendingFrequency` - по возрастанию если `true`, иначе по убыванию.
+
+
+}
