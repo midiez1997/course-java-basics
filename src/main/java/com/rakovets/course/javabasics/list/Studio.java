0330a29dc51d6a6fde67ba68562ec95c356af6e4
Dmitry Sedun <sedun.dima@ya.ru>
2020-10-29 11:19:03 +0300

    add feature for Task Studio

---
+++ src/main/java/com/rakovets/course/javabasics/list/Studio.java
@@ -0,0 +1,40 @@
+package com.rakovets.course.javabasics.list;
+
+public class Studio {
+
+//   private String actors;
+
+//    public static fire() {
+//
+//    }
+
+
+//    public static void main(String[] args) {
+//        List<String> strList = new ArrayList<>();
+//        strList.add("AA");
+//        strList.add("CCCC");
+//        strList.add("BBBBB");
+//        strList.add("Z");
+//        strList.add("EEE");
+//
+//        strList.sort(Comparator.comparingInt(String::length));
+//        strList.forEach(System.out::println);
+//    }
+
+
+//    * `actors` - список актеров, которые работают в студии.
+//
+//Методы:
+//
+//* `fire()` - принимает список актеров и удаляющий из него актера с наибольшим гонораром.
+//
+//=== Specification of task 3
+//
+//Написать компараторы, которые позволят отсортировать список актёров по:
+//
+//* фамилии
+//* возрасту
+//* гонорару
+//* фамилии и возрасту
+//* гонорару и фамилии
+}
