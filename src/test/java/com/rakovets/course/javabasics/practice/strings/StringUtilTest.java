8bd8b5e153300e78eebf237403ea2c2f1d302911
Dmitry Sedun <sedun.dima@ya.ru>
2020-11-02 14:56:04 +0300

    add feature for Task StringUtilTest

--- src/test/java/com/rakovets/course/javabasics/practice/strings/StringUtilTest.java
+++ src/test/java/com/rakovets/course/javabasics/practice/strings/StringUtilTest.java
@@ -12,14 +12,14 @@ public class StringUtilTest {
         stringUtil = new StringUtil(); //создаем объект
     }
 
-    @Test
-    void getUpperCaseTest() { // тренировочный тест
-        //stringUtil.getUpperCase("qwerty"); //у этого объекта мы вызываем метод getUpperCase и передать туда значение " "
-        Assertions.assertEquals("QWERTY", stringUtil.getUpperCase("qwerty")); // мы ожидаем, что этот код вернет вот такой результат
-        Assertions.assertEquals("QWERTY", stringUtil.getUpperCase("QweRty"));
-        Assertions.assertEquals("QW123ERTY", stringUtil.getUpperCase("qw123erty"));
-        Assertions.assertEquals("QW+ERTY", stringUtil.getUpperCase("qw+erty"));
-    }
+//    @Test
+//    void getUpperCaseTest() { // тренировочный тест
+//        //stringUtil.getUpperCase("qwerty"); //у этого объекта мы вызываем метод getUpperCase и передать туда значение " "
+//        Assertions.assertEquals("QWERTY", stringUtil.getUpperCase("qwerty")); // мы ожидаем, что этот код вернет вот такой результат
+//        Assertions.assertEquals("QWERTY", stringUtil.getUpperCase("QweRty"));
+//        Assertions.assertEquals("QW123ERTY", stringUtil.getUpperCase("qw123erty"));
+//        Assertions.assertEquals("QW+ERTY", stringUtil.getUpperCase("qw+erty"));
+//    }
 
     @Test
     void getConcatTest() { // 1
