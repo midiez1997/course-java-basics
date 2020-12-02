6ce6ced9e5e05c5515a0f8a78e5b43109bffaed2
Dmitry Sedun <sedun.dima@ya.ru>
2020-12-02 22:48:57 +0300

    feat: add solution for concurrency utilities

---
+++ src/main/java/com/rakovets/course/javabasics/practice/concurrency/utilities/ArrayMax.java
@@ -0,0 +1,28 @@
+package com.rakovets.course.javabasics.practice.concurrency.utilities;
+
+public class ArrayMax {
+    private Integer[] array;
+    private int max;
+
+    public ArrayMax(Integer[] array, int max) {
+        this.array = array;
+        this.max = max;
+    }
+
+    public Integer[] getArray() {
+        return array;
+    }
+
+    public void setArray(Integer[] array) {
+        this.array = array;
+    }
+
+    public int getMax() {
+        return max;
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+}
---
+++ src/main/java/com/rakovets/course/javabasics/practice/concurrency/utilities/ParallelCalculator.java
@@ -0,0 +1,49 @@
+package com.rakovets.course.javabasics.practice.concurrency.utilities;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ParallelCalculator extends Thread implements Runnable {
+    private Integer[] array;
+    private int max;
+    private static List<ArrayMax> result = Collections.synchronizedList(new ArrayList<>());
+
+    public ParallelCalculator(Integer[] array) {
+        this.array = array;
+    }
+
+    public static List<ArrayMax> calcMax(List<Integer[]> list, int numberOfThreads) {
+        ExecutorService executor = Executors.newFixedThreadPool(numberOfThreads);
+
+        Runnable[] calculators = new ParallelCalculator[numberOfThreads];
+        int threads = 0;
+        for (int i = 0; i < calculators.length; i++) {
+
+            calculators[threads] = new ParallelCalculator(list.get(i));
+            executor.execute(calculators[i]);
+            executor.shutdown();
+
+
+            threads++;
+            if (threads == numberOfThreads) {
+                threads = 0;
+            }
+        }
+
+
+        ArrayList<ArrayMax> res = new ArrayList<>();
+        res.addAll(result);
+        return res;
+    }
+
+    public void run() {
+        Integer[] newArray = array.clone();
+        Arrays.sort(newArray);
+        max = newArray[newArray.length - 1];
+        result.add(new ArrayMax(array, max));
+    }
+
+}
